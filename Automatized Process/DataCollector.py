b888792de7fc2d709a624fd9b31fdc3df9ebce06
kevLopes <kevi_lopes@hotmail.com>
2023-03-25 11:54:24 +0100

    folder name changed

--- Automatized Process/DataCollector.py
+++ Automatized Process/DataCollector.py
@@ -7,6 +7,8 @@ def dataCollectorFuncPiping():
 
     print("Function to capture all Piping Data Initialized")
 
+    global success
+
     # Set the search directory and keyword
     search_dir = os.path.dirname(os.path.abspath(__file__))
     keyword = "Piping"
@@ -19,6 +21,11 @@ def dataCollectorFuncPiping():
 
     # Search for Excel files containing the keyword
     files = [file for file in os.listdir(search_dir) if keyword in file and file.endswith(".xlsx")]
+
+    # Check if any Excel files were found with the specified keyword
+    if not files:
+        print("No excel data file for the Material " + keyword)
+        return
 
     # Check if the Data Organize folder exists, and create it if it doesn't
     output_dir = os.path.join(search_dir, "Materials Data Organized")
@@ -51,5 +58,140 @@ def dataCollectorFuncPiping():
             extract_df.to_excel(output_filename, index=False)
 
             print(f"Extracted data from {file} and saved it to {output_filename}")
+            success = True
         else:
             print(f"Could not find any of the specified columns in {file}")
+            success = False
+
+        if not success:
+            print("No files were processed successfully.")
+
+#TO BE ADJUSTED AND TESTED - VALVES
+def dataCollectorFuncValve():
+
+    global success
+    print("Function to capture all Valves Data Initialized")
+
+    # Set the search directory and keyword
+    search_dir = os.path.dirname(os.path.abspath(__file__))
+    keyword = "Valves"
+
+    # Set the columns to extract
+    columns_to_extract = ["Tag Number", "ID", "Project Number", "Product Code", "Commodity Code",
+                          "Service Description", "Pipe Base Material", "Material", "LineNumber",
+                          "SBM scope", "Total QTY to commit", "Quantity UOM", "Unit Weight",
+                          "Unit Weight UOM", "Total NET weight", "SIZE"]
+
+    # Search for Excel files containing the keyword
+    files = [file for file in os.listdir(search_dir) if keyword in file and file.endswith(".xlsx")]
+
+    # Check if any Excel files were found with the specified keyword
+    if not files:
+        print("No excel data file for the Material " + keyword)
+        return
+
+    # Check if the Data Organize folder exists, and create it if it doesn't
+    output_dir = os.path.join(search_dir, "Materials Data Organized")
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
+    # Loop through the files and extract the specified columns
+    for file in files:
+        # Load the Excel file into a pandas dataframe
+        df = pd.read_excel(os.path.join(search_dir, file), engine='openpyxl')
+
+        # Find the specified columns
+        extract_columns = []
+        for column in df.columns:
+            if any(col in str(column) for col in columns_to_extract):
+                extract_columns.append(column)
+
+        # If any of the specified columns were found, extract them and all rows below with data information
+        if extract_columns:
+            extract_df = df.loc[df[extract_columns].notnull().any(axis=1), extract_columns]
+
+            # Get the project number from the dataframe
+            project_number = extract_df["Project Number"].iloc[0]
+
+            # Get the current timestamp
+            timestamp = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+
+            # Save the extracted data to a new Excel file with the project number in the filename
+            output_filename = os.path.join(output_dir, f"{project_number} - Valves Organized_{timestamp}.xlsx")
+            extract_df.to_excel(output_filename, index=False)
+
+            print(f"Extracted data from {file} and saved it to {output_filename}")
+            success = True
+        else:
+            print(f"Could not find any of the specified columns in {file}")
+            success = False
+
+    if not success:
+        print("No files were processed successfully.")
+
+
+#TO BE ADJUSTED AND TESTED - BOLT
+def dataCollectorFuncBolt():
+
+    print("Function to capture all Bolt Data Initialized")
+
+    global success
+
+    # Set the search directory and keyword
+    search_dir = os.path.dirname(os.path.abspath(__file__))
+    keyword = "Bolt"
+
+    # Set the columns to extract
+    columns_to_extract = ["Tag Number", "ID", "Project Number", "Product Code", "Commodity Code",
+                          "Service Description", "Pipe Base Material", "Material", "LineNumber",
+                          "SBM scope", "Total QTY to commit", "Quantity UOM", "Unit Weight",
+                          "Unit Weight UOM", "Total NET weight", "SIZE"]
+
+    # Search for Excel files containing the keyword
+    files = [file for file in os.listdir(search_dir) if keyword in file and file.endswith(".xlsx")]
+
+    # Check if any Excel files were found with the specified keyword
+    if not files:
+        print("No excel data file for the Material " + keyword)
+        return
+
+    # Check if the Data Organize folder exists, and create it if it doesn't
+    output_dir = os.path.join(search_dir, "Materials Data Organized")
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
+    # Loop through the files and extract the specified columns
+    success = False
+    for file in files:
+        # Load the Excel file into a pandas dataframe
+        df = pd.read_excel(os.path.join(search_dir, file), engine='openpyxl')
+
+        # Find the specified columns
+        extract_columns = []
+        for column in df.columns:
+            if any(col in str(column) for col in columns_to_extract):
+                extract_columns.append(column)
+
+        # If any of the specified columns were found, extract them and all rows below with data information
+        if extract_columns:
+            extract_df = df.loc[df[extract_columns].notnull().any(axis=1), extract_columns]
+
+            # Get the project number from the dataframe
+            project_number = extract_df["Project Number"].iloc[0]
+
+            # Get the current timestamp
+            timestamp = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+
+            # Save the extracted data to a new Excel file with the project number in the filename
+            output_filename = os.path.join(output_dir, f"{project_number} - Bolt Organized_{timestamp}.xlsx")
+            extract_df.to_excel(output_filename, index=False)
+
+            print(f"Extracted data from {file} and saved it to {output_filename}")
+            success = True
+        else:
+            print(f"Could not find any of the specified columns in {file}")
+            success = False
+
+    if not success:
+        print("No files were processed successfully.")
+
--- Automatized Process/main.py
+++ Automatized Process/main.py
@@ -3,8 +3,47 @@ import DataCollector
 import EcosysData_API
 
 if __name__ == '__main__':
+    while True:
+        user_input = input("Do you want to call Pipping Data Collector function? (Y/N)").lower()
 
-    DataCollector.dataCollectorFuncPiping()
+        if user_input == "y":
+            DataCollector.dataCollectorFuncPiping()
+            break
+        elif user_input == "n":
+            break
+        else:
+            print("Invalid input. Please enter 'Y' or 'N'.")
 
-    EcosysData_API.EcosysPOLineAPIData("https://Ecosys.sbm","keven.deOliveiralope","My-SBM#code23")
+    while True:
+        user_input = input("Do you want to call Valve Data Collector function? (Y/N)").lower()
+
+        if user_input == "y":
+            DataCollector.dataCollectorFuncValve()
+            break
+        elif user_input == "n":
+            break
+        else:
+            print("Invalid input. Please enter 'Y' or 'N'.")
+
+    while True:
+        user_input = input("Do you want to call Bolt Data Collector function? (Y/N)").lower()
+
+        if user_input == "y":
+            DataCollector.dataCollectorFuncBolt()
+            break
+        elif user_input == "n":
+            break
+        else:
+            print("Invalid input. Please enter 'Y' or 'N'.")
+
+    while True:
+        user_input = input("Do you want to call Ecosys PO Line API Data function? (Y/N)").lower()
+
+        if user_input == "y":
+            EcosysData_API.EcosysPOLineAPIData("https://Ecosys.sbm","keven.deOliveiralope","My-SBM#code23")
+            break
+        elif user_input == "n":
+            break
+        else:
+            print("Invalid input. Please enter 'Y' or 'N'.")
 
