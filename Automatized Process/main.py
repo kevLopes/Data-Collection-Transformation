9943761c9a16bfac6e97f91e51ffe341becd24ae
kevLopes <kevi_lopes@hotmail.com>
2023-03-27 18:33:16 +0200

    This is a reorganization

--- Automatized Process/main.py
+++ Automatized Process/main.py
@@ -8,8 +8,8 @@ if __name__ == '__main__':
         user_input = input("Do you want to call Ecosys PO Line API Data function? (Y/N)").lower()
 
         if user_input == "y":
-            #user_inputProj = input("For which project do you want to read the data (project number always start with MP)")
-            #EcosysData_API.ecosys_po_lines_data_api(f"https://ecosys-stg.sbmoffshore.com/ecosys/api/restjson/EcosysPOLinesData_DCTAPI_KOL/?RootCostObject={user_inputProj}","keven.deOliveiralope","My-SBM#code23")
+            user_inputProj = input("For which project do you want to read the data (project number always start with MP)")
+            EcosysData_API.ecosys_po_lines_data_api(f"https://ecosys-stg.sbmoffshore.com/ecosys/api/restjson/EcosysPOLinesData_DCTAPI_KOL/?RootCostObject={user_inputProj}","keven.deOliveiralope","My-SBM#code23")
 
             user_inputProj = input("For which project do you want to read the data (project number always start with MP)")
             EcosysData_API.EcosysSUNAPIData(f"https://ecosys-stg.sbmoffshore.com/ecosys/api/restjson/EcosysSUNData_DCTAPI_KOL/?RootCostObject={user_inputProj}","keven.deOliveiralope", "My-SBM#code23")
